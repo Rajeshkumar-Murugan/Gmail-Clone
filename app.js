bdc2d15a73b4f2705e3a902469f88b199d67de7c
Rajeshkumar-Murugan <85731833+Rajeshkumar-Murugan@users.noreply.github.com>
2022-01-03 06:56:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,55 +1,56 @@
 //=========application script starts============
-
-const userdetails = document.querySelector('.row');
-userdetails.innerHTML = ''; //wipping the old data  
-
-userdetails.innerHTML = `<div id="Message">
-  
-<ul>
-  <li>
-      <div id="primaryMessage">
+function applicationload(){
+    const userdetails = document.querySelector('.row');
+    userdetails.innerHTML = ''; //wipping the old data  
+    
+    userdetails.innerHTML = `<div id="Message">
+      
+    <ul>
+      <li>
+          <div id="primaryMessage">
+                
+              <button onclick="getThreadmsg()" class="primaryBtn" id="button">
+                      <i class="material-icons" id="sectionTwoicon">inbox</i> 
+                  <div>
+                      Primary 
+                  </div> 
+              </button>               
+              </div>
             
-          <button onclick="getThreadmsg()" class="primaryBtn" id="button">
-                  <i class="material-icons" id="sectionTwoicon">inbox</i> 
+          </li>
+      <li>
+        <button class="socialBtn" onclick ="getThreadmsg()" id="button">
+          <i class="material-icons" id="sectionTwoicon">send</i> 
               <div>
-                  Primary 
-              </div> 
-          </button>               
-          </div>
-        
-      </li>
-  <li>
-    <button class="socialBtn" onclick ="getThreadmsg()" id="button">
-      <i class="material-icons" id="sectionTwoicon">send</i> 
-          <div>
-              Sent Email
-          </div>
+                  Sent Email
+              </div>
+                  
+              </button>
               
-          </button>
-          
-      </li>
-  <li>
-    
-    
-    <button class="promotionsBtn" onclick="listOfdrafts()" id="button">
-       <i class="material-icons" id="sectionTwoicon">drafts</i>
-   <div>
-      Draft Email
-   </div>             
-          </button>            
-          </li> 
-          </ul>
- 
-</div>   
+          </li>
+      <li>
+        
+        
+        <button class="promotionsBtn" onclick="listOfdrafts()" id="button">
+           <i class="material-icons" id="sectionTwoicon">drafts</i>
+       <div>
+          Draft Email
+       </div>             
+              </button>            
+              </li> 
+              </ul>
      
-    <div >
-      <div class="primary"> </div>
-  </div>
-          
-     <div>
-      <input type="text" placeholder="Enter Email ID" id="emailField">
-      <input type="text" placeholder="Message" id="messageField">
-      <button id="sendBtn">send</button>
-
-     </div>
-      `
+    </div>   
+         
+        <div >
+          <div class="primary"> </div>
+      </div>
+              
+         <div>
+          <input type="text" placeholder="Enter Email ID" id="emailField">
+          <input type="text" placeholder="Message" id="messageField">
+          <button id="sendBtn">send</button>
+    
+         </div>
+          `
+}
